9b96d154da1ff45899b91c8b5b1fa96daedb4f2c
NavrasK <trailmixerstudios@gmail.com>
2023-02-01 18:55:49 -0700

    Added shape class

---
+++ app/src/main/java/com/example/lab_4_codecatchers/Shape.java
@@ -0,0 +1,25 @@
+package com.example.lab_4_codecatchers;
+
+public class Shape {
+    protected int x, y;
+    public Shape() {
+        this.x = 0;
+        this.y = 0;
+    }
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return this.x;
+    }
+    public int getY() {
+        return this.y;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+}

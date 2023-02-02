6c69bd34698705829a2fa81b0330f159b5188044
NavrasK <trailmixerstudios@gmail.com>
2023-02-01 18:55:49 -0700

    Added shape class

--- app/src/main/java/com/example/lab_4_codecatchers/Shape.java
+++ app/src/main/java/com/example/lab_4_codecatchers/Shape.java
@@ -2,9 +2,12 @@ package com.example.lab_4_codecatchers;
 
 public class Shape {
     protected int x, y;
+    protected String color;
+
     public Shape() {
         this.x = 0;
         this.y = 0;
+        this.color = "blue";
     }
     public Shape(int x, int y) {
         this.x = x;
